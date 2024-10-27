1f4f068694022c38fdc39e19d3348f44db3557c0
Oscar Ruiz <oscarruiz2614@gmail.com>
2024-10-26 19:15:05 -0500

    fix: :bug: correcion en el AppConfig

--- src/main/java/com/example/hulk_store_backend/config/AppConfig.java
+++ src/main/java/com/example/hulk_store_backend/config/AppConfig.java
@@ -3,7 +3,9 @@ package com.example.hulk_store_backend.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
