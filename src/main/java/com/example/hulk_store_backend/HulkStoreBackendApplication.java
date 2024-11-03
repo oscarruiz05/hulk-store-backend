2ed891289bff76de4c20f9d4f0771e91fcd4025f
Oscar Ruiz <oscarruiz2614@gmail.com>
2024-11-03 00:51:47 -0500

    feature: :sparkles: implementacion de url global

--- src/main/java/com/example/hulk_store_backend/HulkStoreBackendApplication.java
+++ src/main/java/com/example/hulk_store_backend/HulkStoreBackendApplication.java
@@ -2,8 +2,10 @@ package com.example.hulk_store_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@RequestMapping("/api")
 public class HulkStoreBackendApplication {
 
 	public static void main(String[] args) {
